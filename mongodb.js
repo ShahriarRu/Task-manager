aa1e2d75d3a21fb5b9153617c5c22b0753bebc1a
Shahriar Alif <shahriarcse26@gmail.com>
2021-07-08 15:05:40 +0600

    mongodb database connected

---
+++ mongodb.js
@@ -0,0 +1,23 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+
+    console.log("Datebase Connected Successfully...");
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne({
+      name: "Shahriar",
+      age: 22,
+    });
+  }
+);
