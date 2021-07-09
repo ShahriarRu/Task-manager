bcc369f0e146e57ba287a7b48cc42c4ca6eabff9
Shahriar Alif <shahriarcse26@gmail.com>
2021-07-09 21:00:57 +0600

    Mongoose added

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,8 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+const ObjectID = mongodb.ObjectID;
+
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
 
@@ -15,9 +17,89 @@ MongoClient.connect(
     console.log("Datebase Connected Successfully...");
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Shahriar",
-      age: 22,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Shahriar",
+    //     age: 22,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Shahriar",
+    //       age: 22,
+    //     },
+    //     {
+    //       name: "Shakib",
+    //       age: 25,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    //
+    // db.collection("users").findOne({ name: "Shakib" }, (error, user) => {
+    //   if (error) {
+    //     return console.log("Unable to find the user");
+    //   }
+    //   console.log(user);
+    // });
+
+    // db.collection("tasks")
+    //   .find({ completed: true })
+    //   .toArray((error, tasks) => {
+    //     if (error) {
+    //       return console.log("Unabale to find task");
+    //     }
+    //     console.log(tasks);
+    //   });
+
+    // db.collection("users")
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID("60e6bc71d3e7434154a7b0a6"),
+    //     },
+    //     {
+    //       $set: {
+    //         name: "Rakib",
+    //       },
+    //     }
+    //   )
+    //   .then((result) => {
+    //     console.log(result);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    db.collection("tasks")
+      .updateMany(
+        {
+          completed: false,
+        },
+        {
+          $set: {
+            completed: true,
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result.modifiedCount);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 );
