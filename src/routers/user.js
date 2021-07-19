4e18299557d92e94f6c3deaac0e3c35160cb7dfa
Shahriar Alif <shahriarcse26@gmail.com>
2021-07-19 15:46:12 +0600

    login-logout added

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const User = require("../models/user");
 const router = new express.Router();
+const auth = require("../middleware/auth");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
@@ -19,19 +21,37 @@ router.post("/users/login", async (req, res) => {
       req.body.email,
       req.body.password
     );
-    res.send(user);
+
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (e) {
     res.status(400).send();
   }
 });
 
-router.get("/users", async (req, res) => {
+router.post("/users/logout", async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
+    await req.user.save();
+    res.send();
   } catch (e) {
-    res.status(500).send();
+    res.status(400).send(e);
   }
+});
+
+// router.get("/users", auth, async (req, res) => {
+//   try {
+//     const users = await User.find({});
+//     res.send(users);
+//   } catch (e) {
+//     res.status(500).send();
+//   }
+// });
+
+router.get("/users/me", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users/:id", async (req, res) => {
